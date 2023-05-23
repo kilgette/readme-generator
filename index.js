f428a5214f8215a173c3bdbd8b6e86f6f7ad01e5
kilgette <kilgette@gmail.com>
2023-05-23 18:48:01 -0500

    test

--- index.js
+++ index.js
@@ -20,21 +20,51 @@ inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      name: 'projectTitle',
+      message: 'Enter the project title:',
     },
     {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+      type: 'input',
+      name: 'description',
+      message: 'Enter the project description:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Enter installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Enter usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Enter contribution guidelines:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Enter test instructions:',
     },
     {
       type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'telekinesis'],
+      message: 'Choose a license:',
+      name: 'license',
+      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None',],
     },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Enter your Github username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+   
   ])
   .then((data) => {
     console.log(data);
