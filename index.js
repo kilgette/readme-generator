e7bf965d49463d27169827d3c944815cc2638f1d
kilgette <kilgette@gmail.com>
2023-05-23 19:34:17 -0500

    file frustration

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const text = `
-Lorem ipsum dolor sit amet, 
+const text = `This is a ReadMe Generator. Please follow the prompts below.`
 
-consectetur adipiscing elit, sed do eiusmod tempor 
-
-incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-
-
-
-
-
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-`
 // $()
 console.log(text);
+
+// function promptUser() {
+//   return new Promise((resolve, reject) => {
 
 inquirer
   .prompt([
@@ -64,27 +56,16 @@ inquirer
       name: 'email',
       message: 'Enter your email address:',
     },
-   
   ])
+
   .then((data) => {
     console.log(data);
-    // const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+    writeToFile(generateMarkdown(data));
+
 
     fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
       err ? console.log(err) : console.log('Success!')
     );
   });
 
-//   // TODO: Include packages needed for this application
-
-// // TODO: Create an array of questions for user input
-// const questions = [];
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
-
-// // TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
+  
